52e219a3e56d2d90b8f4d1fd11f113ac409a5951
tigranium123 <tigranium123@gmail.com>
2023-11-08 14:03:34 +0400

    add container uri

--- instance/config.py
+++ instance/config.py
@@ -1,1 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Q!w2e3r4t5@localhost:5433/dev'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Q!w2e3r4t5@localhost:5433/dev'
+SQLALCHEMY_DATABASE_URI = 'postgresql://docker:docker@postgres_db:5432/exampledb'
