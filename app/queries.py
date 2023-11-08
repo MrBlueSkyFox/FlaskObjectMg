ef0b47dc0bbe91386d70b0cfeda5eef8d03d8c31
tigranium123 <tigranium123@gmail.com>
2023-11-08 14:05:45 +0400

    clean print

--- app/queries.py
+++ app/queries.py
@@ -3,9 +3,7 @@ from .object_mgr import ObjectMgr
 
 def get_object(_, info):
     try:
-        print("zzs")
         obj = ObjectMgr.get_object()
-        print(obj)
         payload = obj.to_dict()
     except Exception as e:
         payload = None
